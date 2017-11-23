65cd75e803ede160d0545095bb648f77485a3a05
Rolloff, Artur <Artur.Rolloff@opitz-consulting.com>
2017-11-23 11:25:23 +0100

    tabelle logs angepasst. es fehlt noch z.b. der primär-schlüssel..

--- backend/tables/logs.sql
+++ backend/tables/logs.sql
@@ -1,33 +1,39 @@
 create table logs (
-  logs_id      number(10) not null,
-  erstellt_am  timestamp(6) not null,
-  erstellt_von varchar2(30) not null,
-  stufe        varchar2(5) not null,
-  meldung      varchar2(32767) not null,
-  objekt_name  varchar2(30) not null,
-  modul_name   varchar2(61) not null, --hier will ich auch sehen: <package.procedure|function>
-  zeile        number(6) not null
+  logs_id       number(10)    not null,
+  erstellt_am   timestamp(6)  not null,
+  erstellt_von  varchar2(32)  not null,
+  geaendert_am  timestamp(6)  not null,
+  geaendert_von varchar2(32)  not null,
+  version       number(10)    not null,
+  zeitstempel   timestamp(6)  not null,
+  stufe         varchar2(5)   not null,
+  modul         varchar2(61)  not null,
+  zeile         number(6)     not null,
+  meldung       varchar2(255) not null
 )
 /
 
-comment on table logsaetze is 'Tabelle Logsaetze zum Festhalten der Log-Meldungen.'
+comment on table logs is 'Tabelle Logsaetze zum Festhalten der Log-Meldungen.'
 /
-comment on column logsaetze.logs_id is 'Primärschlüssel.'
+comment on column logs.logs_id is 'Primärschlüssel.'
 /
-comment on column logsaetze.erstellt_am is 'Zeitpunkt an dem die Log-Meldung erstellt wurde.'
+comment on column logs.erstellt_am is 'Zeitpunkt an dem der Eintrag erstellt wurde.'
 /
-comment on column logsaetze.erstellt_von is 'Aktuelle Benutzer zur Zeit der Erstellung der Log-Meldung.'
+comment on column logs.erstellt_von is 'Aktuelle Benutzer zur Zeit der Erstellung des Eintrags.'
 /
-comment on column logsaetze.stufe is 'Log-Stufe (INFO, DEBUG, WARN, ERROR, FATAL).'
+comment on column logs.geaendert_am is 'Zeitpunkt an dem der Eintrag geändert wurde.'
 /
-comment on column logsaetze.meldung is 'Log-Meldung.'
+comment on column logs.geaendert_von is 'Aktuelle Benutzer zur Zeit der Änderung des Eintrags.'
 /
-comment on column logsaetze.schema_name is 'Aktuelles DB-Schema.'
+comment on column logs.version is 'Version des Eintrags.'
 /
-comment on column logsaetze.objekt_name is 'Typ des Objekts in dem sich die Programmeinheit befindet z.B. FUNCTION, PACKAGE BODY, TYPE, usw.'
+comment on column logs.zeitstempel is 'Zeitpunkt an dem der Log-Eintrag erstellt wurde.'
 /
-comment on column logsaetze.modul_name is 'Programmeinheit z.B. eine konkrete Prozedur oder Funktion in der die Log-Meldung ausgelöst wurde.'
+comment on column logs.stufe is 'Log-Stufe (INFO, DEBUG, WARN, ERROR).'
 /
-comment on column logsaetze.zeile is 'Zeilennummer in der die Log-Meldung ausgelöst wurde.'
+comment on column logs.modul is 'Programmeinheit z.B. eine konkrete Prozedur oder Funktion in der die Log-Meldung ausgelöst wurde.'
 /
-
+comment on column logs.zeile is 'Zeilennummer in der die Log-Meldung ausgelöst wurde.'
+/
+comment on column logs.meldung is 'Log-Meldung.'
+/
