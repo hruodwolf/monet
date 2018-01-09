22cdf96dc5c27cf23d3eb0b5e6f23a2474b6390d
Rolloff, Artur <Artur.Rolloff@opitz-consulting.com>
2018-01-09 18:57:34 +0100

    daten gesammelt für spaltenbreite

---
+++ backend/tables/camt_export.sql
@@ -0,0 +1,20 @@
+Daten sammeln um auf Spaltenwerte zukommen
+
+Jahr 2017		
+Auftragskonto	9	
+Buchungstag	5	
+Valutadatum	5	
+Buchungstext	25
+Verwendungszweck		128
+Glaeubiger ID 	26
+Mandatsreferenz	 35
+Kundenreferenz (End-to-End) 35
+Sammlerreferenz	0
+Lastschrift	Ursprungsbetrag 0
+Auslagenersatz Ruecklastschrift	0
+Beguenstigter/Zahlungspflichtiger	91
+Kontonummer/IBAN	22
+BIC (SWIFT-Code) 11
+Betrag	7
+Waehrung 3
+Info 14
